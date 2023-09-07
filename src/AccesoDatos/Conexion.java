cc08ef45d3b9eeb5fb1f0fb05924a761606f09f5
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2023-09-07 12:47:24 -0300

    Actualizacion

--- src/AccesoDatos/Conexion.java
+++ src/AccesoDatos/Conexion.java
@@ -1,7 +1,40 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package AccesoDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
+/**
+ *
+ * @author Jeremias
+ */
 public class Conexion {
-    
+    private static Conexion singleton;
+    public Conexion(){
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Error al cargar los drivers");
+        }
+    }
+    public static Connection obtenerConexion(){
+           Connection con=null;
+        if(singleton==null){
+            singleton=new Conexion();
+        }
+        try {
+//            con=DriverManager.getConnection();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos");
+        }
+        return con;
+    }
 }
--- src/Entidades/Materia.java
+++ src/Entidades/Materia.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Entidades;
 
 /**
@@ -6,5 +10,61 @@ package Entidades;
  * @author Jeremias
  */
 public class Materia {
+    private int idMateria,year;
+    private String nombre;
+    private boolean estado;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, int year, String nombre, boolean estado) {
+        this.idMateria = idMateria;
+        this.year = year;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public Materia(int year, String nombre, boolean estado) {
+        this.year = year;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", year=" + year + ", nombre=" + nombre + '}';
+    }
     
 }
