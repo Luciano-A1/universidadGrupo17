e84a8fa03bf50d62511be30f4b1ff31f6b7b5157
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2023-09-08 10:47:31 -0300

    Conexion1

--- src/AccesoDatos/Conexion1.java
+++ src/AccesoDatos/Conexion1.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AccesoDatos;
 
-/**
- *
- * @author lucia
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class Conexion1 {
-    
+
+    private static String url = "jdbc:mariadb://localhost/guia4_ejercicio1";
+    private static String usuario = "root";
+    private static String password = "";
+
+    private static Conexion1 conexion = null;
+
+    private Conexion1() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Clase Conexion: Error al cargar Driver");
+        }
+    }
+
+    public static Connection getConexion() {
+        Connection con = null;
+        if (conexion == null) {
+
+            conexion = new Conexion1();
+        }
+        try {
+            // Setup the connection with the DB
+            con = DriverManager.getConnection(url + "?useLegacyDatetimeCode=false&serverTimezone=UTC" + "&user=" + usuario + "&password=" + password);
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion ");
+        }
+
+        return con;
+    }
 }
--- src/Entidades/Inscripcion.java
+++ src/Entidades/Inscripcion.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Entidades;
 
-/**
- *
- * @author Jeremias
- */
 public class Inscripcion {
     private int idInscripcion,nota;
     private Alumno alumno;
