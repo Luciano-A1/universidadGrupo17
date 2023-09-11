086175f33789c90d0317014139bb4387c449df0e
Luciano-A1 <141652211+Luciano-A1@users.noreply.github.com>
2023-09-11 12:46:48 -0300

    Atributos ---> MateriaData

--- src/AccesoDatos/MateriaDatos.java
+++ src/AccesoDatos/MateriaDatos.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package AccesoDatos;
 
-/**
- *
- * @author lucia
- */
+import Entidades.Materia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MateriaDatos {
-    
+    private static Connection con = Conexion1.getConexion();
+    private static PreparedStatement ps = null;
+    private static ResultSet rs = null;
+    private static List<Materia> listaM = new ArrayList<>();
 }
