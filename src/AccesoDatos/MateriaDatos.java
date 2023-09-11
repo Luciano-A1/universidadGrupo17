fa50626b4a7522afb67941179cc6c14d1d019a9e
Juan Pablo Hernandez <juanpablohdez87@gmail.com>
2023-09-11 13:21:19 -0300

    juan
    se agrego métodos:
    - guardarMateria();
    -buscarMateriaporID();

--- src/AccesoDatos/MateriaDatos.java
+++ src/AccesoDatos/MateriaDatos.java
@@ -1,16 +1,74 @@
-
 package AccesoDatos;
 
 import Entidades.Materia;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class MateriaDatos {
+
     private static Connection con = Conexion1.getConexion();
     private static PreparedStatement ps = null;
     private static ResultSet rs = null;
     private static List<Materia> listaM = new ArrayList<>();
+
+    public static void guardarMateria(Materia materia) {
+
+        String sql = "insert into materia(nombre, año, estado) value(?, ?, ?)";
+        try {
+            ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getYear());
+            ps.setBoolean(3, materia.isEstado());
+
+            ps.executeUpdate();
+            rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                materia.setIdMateria(rs.getInt(1));
+                System.out.println("Se agrego con exito al ID");
+            } else {
+                System.out.println("no se obtuvo al ID");
+            }
+            ps.close();
+            JOptionPane.showMessageDialog(null, ">>> Guardado <<<");
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al insertar " + ex.getMessage());
+
+        }
+
+    }
+
+    public static void buscarMateriaPorId(int id) {
+
+        String sql = "select nombre, año from materia where idMateria = ? and estado = 1";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Materia materia = new Materia();
+                materia.setIdMateria(id);
+                materia.setNombre(rs.getString("nombre"));
+                materia.setYear(rs.getInt("año"));
+                materia.setEstado(true);
+                System.out.println(materia);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Materia no encontrada");
+            }
+            ps.close();
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error: Acceso Materia");
+        }
+
+    }
+
 }
--- src/universidadgrupo17/UniversidadGrupo17.java
+++ src/universidadgrupo17/UniversidadGrupo17.java
@@ -1,26 +1,40 @@
 package universidadgrupo17;
 
 import AccesoDatos.AlumnosDatos;
+import AccesoDatos.MateriaDatos;
 import Entidades.Alumno;
+import Entidades.Materia;
 import java.time.LocalDate;
 
 public class UniversidadGrupo17 {
 
-
     public static void main(String[] args) {
-        
+
         Alumno alu1 = new Alumno(41885655, "Ledesma", "Jeremias", LocalDate.of(1998, 12, 24), true);
         Alumno alu2 = new Alumno(41885865, "Hernandez", "Juan", LocalDate.of(1998, 2, 24), false);
         Alumno alu3 = new Alumno(42285655, "Bonacci", "Guadalupe", LocalDate.of(1998, 6, 24), true);
         Alumno alu4 = new Alumno(41885677, "Molina", "Luciano", LocalDate.of(1998, 1, 14), true);
 //        Alumno alu5 = new Alumno(7, 41995677, "Galeassi", "Maria", LocalDate.of(1998, 4, 14), true);
 //        AlumnosDatos.modicarAlumno(alu5);
-        
-        
+//        AlumnosDatos.guardarAlumno(alu1);
+//        AlumnosDatos.guardarAlumno(alu2);
+//        AlumnosDatos.guardarAlumno(alu3);
+//        AlumnosDatos.guardarAlumno(alu4);
+
 //        AlumnosDatos.listaAlumno();
-        AlumnosDatos.eliminarAlumno(7);
-        AlumnosDatos.buscarAlumnosPorId(7);
-        
+//        AlumnosDatos.eliminarAlumno(7);
+//        AlumnosDatos.buscarAlumnosPorId(7);
+//------------------------------------------------------------------------------
+        Materia m1 = new Materia(1998, "Matematicas", true);
+        Materia m2 = new Materia(1998, "Historia", true);
+        Materia m3 = new Materia(1998, "Comunicaión", false);
+        Materia m4 = new Materia(1997, "Programación I", true);
+
+//        MateriaDatos.guardarMateria(m1);
+//        MateriaDatos.guardarMateria(m2);
+//        MateriaDatos.guardarMateria(m3);
+//        MateriaDatos.guardarMateria(m4);
+//        MateriaDatos.buscarMateriaPorId(4);
     }
-    
+
 }
